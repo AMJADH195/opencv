30f2c8044c30b6027bfef0aa3080d6acfcde36e6
AMJADH195 <43226290+AMJADH195@users.noreply.github.com>
2019-01-08 01:24:26 -0800

    Add files via upload

---
+++ dbconfig.php
@@ -0,0 +1,12 @@
+<?php
+/*for 360 iot smart energy saving module*/
+
+
+define('DB_USER',"id8416706_smartenergy");
+define('DB_PASSWORD',"smartenergy");
+define('DB_DATABASE',"id8416706_smartenergy");
+define('DB_SERVER',"localhost");
+
+
+
+?>
